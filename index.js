2addd36827e0e9980396182e365250797d7c90d8
Mario maio <mariomaio92@gmail.com>
2024-07-25 22:51:15 +0000

    errore db corretto

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoCl }= require ('mongodb');
+const { MongoClient }= require ('mongodb');
 const dns = require('dns');
 const urlparser = require('url')
 
-const myMongo = new MongoCl ('mongodb+srv://new-user-mario:s7lWU1Oj8GyuctpW@cluster0.6yumhe1.mongodb.net/urlshortner?retryWrites=true&w=majority&appName=Cluster0')
+const myMongo = new MongoClient ('mongodb+srv://new-user-mario:s7lWU1Oj8GyuctpW@cluster0.6yumhe1.mongodb.net/urlshortner?retryWrites=true&w=majority&appName=Cluster0')
 
 
 const db = myMongo.db('urlshortner');
