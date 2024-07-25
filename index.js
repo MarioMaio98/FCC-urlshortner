703213bead61eaaad6ea36331142a0129e280311
Mario maio <mariomaio92@gmail.com>
2024-07-25 22:58:42 +0000

    modifica  parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -16,8 +17,9 @@ const urls = db.collection('urls');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -25,6 +27,7 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
 
 
 
